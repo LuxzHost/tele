6eebcb6a66117ee0d81bf8226d8d19f1b8e3cca7
Luxz - Official <luzfimax@gmail.com>
2025-08-07 23:22:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  TOKEN: '8056868910:AAFc3eXnsXFG4F9gtwFyVcbrxLhIJKigPfo',        // Ganti token bot lu
+  TOKEN: '8219074368:AAFQ1QCg1MGcZM72jaNKxpAhhw4cThC9UDk',        // Ganti token bot lu
   OWNER_ID: 8152395569           // Ganti ID Telegram owner
 };
